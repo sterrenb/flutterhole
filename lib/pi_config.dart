e5e64dbac43dc16b87f8d639632e3b0091a26e29
Thomas <th.sterrenburg@gmail.com>
2019-03-12 15:50:45 +0100

    Feature/flexible uri (#35)
    * Adding end drawer for quick config selection, resolves #30
    
    * Adding SSL toggle amd flexible URI, resolves #31
    
    * Adding snackbars, resolves #32

--- lib/pi_config.dart
+++ lib/pi_config.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sterrenburg.github.flutterhole/widgets/app_state.dart';
+import 'package:sterrenburg.github.flutterhole/widgets/dashboard/snack_bar.dart';
 import 'package:sterrenburg.github.flutterhole/widgets/preferences/preference_is_dark.dart';
 
 class PiConfig {
@@ -41,8 +43,7 @@ class PiConfig {
     return (didSet == null) ? false : didSet;
   }
 
-  Future<bool> switchConfig(
-      {@required BuildContext context, int index = 0, bool pop = true}) async {
+  Future<bool> switchConfig({@required BuildContext context, int index = 0, bool pop = true}) async {
     final bool didSet = await setActiveIndex(index);
     if (!didSet) {
       if (pop) Navigator.pop(context);
@@ -54,7 +55,7 @@ class PiConfig {
     bool isDark = await PreferenceIsDark().get();
     PreferenceIsDark.applyTheme(context, isDark);
     String activeString = await getActiveString();
-    Fluttertoast.showToast(msg: 'Switching to $activeString');
+    showSnackBar(context, 'Switched to $activeString');
     // TODO move up pop
     if (pop) Navigator.pop(context);
     return true;
@@ -79,7 +80,7 @@ class PiConfig {
   Future<int> addNew(String name) async {
     return setConfig(name)
         .catchError((e) {
-      Fluttertoast.showToast(msg: e.toString());
+      showSnackBar(null, e.toString());
     });
   }
 
