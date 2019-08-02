4a33efb479d7100d3ebcca5b8fc925519dbe99a5
sterrenburg <th.sterrenburg@gmail.com>
2019-08-02 17:00:38 +0200

    Toggle button widget test

--- test/widget/status_icon_test.dart
+++ test/widget/status_icon_test.dart
@@ -20,91 +20,120 @@ void main() {
   });
 
   testWidgets('StatusIcon', (WidgetTester tester) async {
-    final child = StatusIcon(color: Colors.purple);
-    materialApp = MockMaterialApp(child: child);
+    materialApp = MockMaterialApp(child: StatusIcon(color: Colors.purple));
     await tester.pumpWidget(materialApp);
 
-    expect((find.byWidget(child).evaluate().first.widget as StatusIcon).color,
+    expect(
+        (find
+            .byType(StatusIcon)
+            .evaluate()
+            .first
+            .widget as StatusIcon).color,
         Colors.purple);
   });
 
   group('VersionsStatusIcon', () {
+    setUp(() {
+      materialApp = MockMaterialApp(child: VersionsStatusIcon());
+    });
+
     testWidgets('grey for BlocStateLoading<Versions>',
-        (WidgetTester tester) async {
-      materialApp = MockMaterialApp(child: VersionsStatusIcon());
-      when(materialApp.versionsBloc.currentState)
-          .thenAnswer((_) => BlocStateLoading<Versions>());
-      await tester.pumpWidget(materialApp);
+            (WidgetTester tester) async {
+          when(materialApp.versionsBloc.currentState)
+              .thenAnswer((_) => BlocStateLoading<Versions>());
+          await tester.pumpWidget(materialApp);
 
-      expect(
-          (find.byType(StatusIcon).evaluate().first.widget as StatusIcon).color,
-          Colors.grey);
-    });
+          expect(
+              (find
+                  .byType(StatusIcon)
+                  .evaluate()
+                  .first
+                  .widget as StatusIcon).color,
+              Colors.grey);
+        });
     testWidgets('green for BlocStateSuccess<Versions>',
-        (WidgetTester tester) async {
-      materialApp = MockMaterialApp(child: VersionsStatusIcon());
-      when(materialApp.versionsBloc.currentState)
-          .thenAnswer((_) => BlocStateSuccess<Versions>(mockVersions));
-      await tester.pumpWidget(materialApp);
+            (WidgetTester tester) async {
+          when(materialApp.versionsBloc.currentState)
+              .thenAnswer((_) => BlocStateSuccess<Versions>(mockVersions));
+          await tester.pumpWidget(materialApp);
 
-      expect(
-          (find.byType(StatusIcon).evaluate().first.widget as StatusIcon).color,
-          Colors.green);
-    });
+          expect(
+              (find
+                  .byType(StatusIcon)
+                  .evaluate()
+                  .first
+                  .widget as StatusIcon).color,
+              Colors.green);
+        });
 
     testWidgets('red for BlocStateError<Versions>',
-        (WidgetTester tester) async {
-      materialApp = MockMaterialApp(child: VersionsStatusIcon());
-      when(materialApp.versionsBloc.currentState)
-          .thenAnswer((_) => BlocStateError<Versions>());
-      await tester.pumpWidget(materialApp);
+            (WidgetTester tester) async {
+          when(materialApp.versionsBloc.currentState)
+              .thenAnswer((_) => BlocStateError<Versions>());
+          await tester.pumpWidget(materialApp);
 
-      expect(
-          (find.byType(StatusIcon).evaluate().first.widget as StatusIcon).color,
-          Colors.red);
-    });
+          expect(
+              (find
+                  .byType(StatusIcon)
+                  .evaluate()
+                  .first
+                  .widget as StatusIcon).color,
+              Colors.red);
+        });
   });
 
   group('ActiveStatusIcon', () {
+    setUp(() {
+      materialApp = MockMaterialApp(child: ActiveStatusIcon());
+    });
+
     testWidgets('grey for BlocStateLoading<Status>',
-        (WidgetTester tester) async {
-      materialApp = MockMaterialApp(child: ActiveStatusIcon());
-      when(materialApp.statusBloc.currentState)
-          .thenAnswer((_) => BlocStateLoading<Status>());
-      await tester.pumpWidget(materialApp);
+            (WidgetTester tester) async {
+          when(materialApp.statusBloc.currentState)
+              .thenAnswer((_) => BlocStateLoading<Status>());
+          await tester.pumpWidget(materialApp);
 
-      expect(
-          (find.byType(StatusIcon).evaluate().first.widget as StatusIcon).color,
-          Colors.grey);
-    });
+          expect(
+              (find
+                  .byType(StatusIcon)
+                  .evaluate()
+                  .first
+                  .widget as StatusIcon).color,
+              Colors.grey);
+        });
     testWidgets('green for BlocStateSuccess<Status> enabled',
-        (WidgetTester tester) async {
-      materialApp = MockMaterialApp(child: ActiveStatusIcon());
-      when(materialApp.statusBloc.currentState)
-          .thenAnswer((_) => BlocStateSuccess<Status>(mockStatusEnabled));
-      await tester.pumpWidget(materialApp);
+            (WidgetTester tester) async {
+          when(materialApp.statusBloc.currentState)
+              .thenAnswer((_) => BlocStateSuccess<Status>(mockStatusEnabled));
+          await tester.pumpWidget(materialApp);
 
-      expect(
-          (find.byType(StatusIcon).evaluate().first.widget as StatusIcon).color,
-          Colors.green);
-    });
+          expect(
+              (find
+                  .byType(StatusIcon)
+                  .evaluate()
+                  .first
+                  .widget as StatusIcon).color,
+              Colors.green);
+        });
 
     testWidgets('orange for BlocStateSuccess<Status> disabled',
-        (WidgetTester tester) async {
-      materialApp = MockMaterialApp(child: ActiveStatusIcon());
-      when(materialApp.statusBloc.currentState)
-          .thenAnswer((_) => BlocStateSuccess<Status>(mockStatusDisabled));
-      await tester.pumpWidget(materialApp);
+            (WidgetTester tester) async {
+          when(materialApp.statusBloc.currentState)
+              .thenAnswer((_) => BlocStateSuccess<Status>(mockStatusDisabled));
+          await tester.pumpWidget(materialApp);
 
-      expect(
-          (find.byType(StatusIcon).evaluate().first.widget as StatusIcon).color,
-          Colors.orange);
-    });
+          expect(
+              (find
+                  .byType(StatusIcon)
+                  .evaluate()
+                  .first
+                  .widget as StatusIcon).color,
+              Colors.orange);
+        });
 
     testWidgets('orange for StatusStateSleeping', (WidgetTester tester) async {
-      materialApp = MockMaterialApp(child: ActiveStatusIcon());
       when(materialApp.statusBloc.currentState).thenAnswer(
-          (_) => StatusStateSleeping(Duration(seconds: 1), Stopwatch()));
+              (_) => StatusStateSleeping(Duration(seconds: 1), Stopwatch()));
       await tester.pumpWidget(materialApp);
 
       expect(
@@ -113,7 +142,6 @@ void main() {
     });
 
     testWidgets('red for BlocStateError<Status>', (WidgetTester tester) async {
-      materialApp = MockMaterialApp(child: ActiveStatusIcon());
       when(materialApp.statusBloc.currentState)
           .thenAnswer((_) => BlocStateError<Status>());
       await tester.pumpWidget(materialApp);
