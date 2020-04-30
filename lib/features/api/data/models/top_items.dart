f139453fd15bca302640de3a312a080f600301ad
thomas-sterrenburg <th.sterrenburg@gmail.com>
2020-04-30 23:53:09 +0200

    Adding top items

---
+++ lib/features/api/data/models/top_items.dart
@@ -0,0 +1,18 @@
+import 'package:flutterhole/features/api/data/models/model.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'top_items.freezed.dart';
+
+part 'top_items.g.dart';
+
+/// {{ base_url  }}?topItems&auth={{ auth  }}
+@freezed
+abstract class TopItems extends Model with _$TopItems {
+  const factory TopItems({
+    @required @JsonKey(name: 'top_queries') Map<String, int> topQueries,
+    @required @JsonKey(name: 'top_ads') Map<String, int> topAds,
+  }) = _TopItems;
+
+  factory TopItems.fromJson(Map<String, dynamic> json) =>
+      _$TopItemsFromJson(json);
+}
--- test/model_test.dart
+++ test/model_test.dart
@@ -4,6 +4,7 @@ import 'package:flutterhole/features/api/data/models/dns_query_type.dart';
 import 'package:flutterhole/features/api/data/models/model.dart';
 import 'package:flutterhole/features/api/data/models/over_time_data.dart';
 import 'package:flutterhole/features/api/data/models/summary.dart';
+import 'package:flutterhole/features/api/data/models/top_items.dart';
 
 import 'fixture_reader.dart';
 
@@ -26,10 +27,11 @@ void testModel<T extends Model>(
 }
 
 void main() {
-  testModel<Summary>('summary.json', (json) => Summary.fromJson(json));
+  testModel<Summary>('summaryRaw.json', (json) => Summary.fromJson(json));
   testModel<DnsQueryType>(
       'singleDnsQueryType.json', (json) => DnsQueryType.fromJson(json));
   testModel<DnsQueryTypeResult>(
       'getQueryTypes.json', (json) => DnsQueryTypeResult.fromJson(json));
   testModel<OverTimeData>('overTimeData10mins.json', (json) => OverTimeData.fromJson(json));
+  testModel<TopItems>('topItems.json', (json) => TopItems.fromJson(json));
 }
