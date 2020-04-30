2dd56c828207448b163136c906598e61d978a19a
thomas-sterrenburg <th.sterrenburg@gmail.com>
2020-05-01 00:05:18 +0200

    Broken missing summary attributes

--- test/model_test.dart
+++ test/model_test.dart
@@ -3,15 +3,17 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutterhole/features/api/data/models/dns_query_type.dart';
 import 'package:flutterhole/features/api/data/models/model.dart';
 import 'package:flutterhole/features/api/data/models/over_time_data.dart';
+import 'package:flutterhole/features/api/data/models/pi_client.dart';
 import 'package:flutterhole/features/api/data/models/summary.dart';
 import 'package:flutterhole/features/api/data/models/top_items.dart';
+import 'package:flutterhole/features/api/data/models/top_sources.dart';
 
 import 'fixture_reader.dart';
 
 void testModel<T extends Model>(
     String fixtureName, Function1<Map<String, dynamic>, T> fromJson) {
   test(
-    '$T should be cyclical',
+    '$T toJson/fromJson should be cyclical',
     () async {
       // arrange
       final Map<String, dynamic> json = jsonFixture('$fixtureName');
@@ -19,8 +21,8 @@ void testModel<T extends Model>(
       final T model = fromJson(json);
       final map = model.toJson();
       // assert
-      map.forEach((key, value) {
-        expect(json, containsPair(key, value));
+      json.forEach((key, value) {
+        expect(map, containsPair(key, value));
       });
     },
   );
@@ -32,6 +34,10 @@ void main() {
       'singleDnsQueryType.json', (json) => DnsQueryType.fromJson(json));
   testModel<DnsQueryTypeResult>(
       'getQueryTypes.json', (json) => DnsQueryTypeResult.fromJson(json));
-  testModel<OverTimeData>('overTimeData10mins.json', (json) => OverTimeData.fromJson(json));
+  testModel<OverTimeData>(
+      'overTimeData10mins.json', (json) => OverTimeData.fromJson(json));
   testModel<TopItems>('topItems.json', (json) => TopItems.fromJson(json));
+  testModel<PiClient>('singlePiClient.json', (json) => PiClient.fromJson(json));
+  testModel<TopSourcesResult>(
+      'getQuerySources.json', (json) => TopSourcesResult.fromJson(json));
 }
